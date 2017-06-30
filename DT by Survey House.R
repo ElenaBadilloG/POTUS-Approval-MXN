e6f203dcecfdc99043cbeda5eb9720f43d41a4a0
Elena Badillo <elena.badillo.g@gmail.com>
2017-06-30 16:15:54 -0500

    Update DT by Survey House.R

--- DT by Survey House.R
+++ DT by Survey House.R
@@ -1,12 +1,16 @@
-
 #######################################################################################################################################################
 ########################################  Trump Approval by Survey House #############################################################################
 ############################################### Elena Badillo Goicoechea, June 21 2017 ################################################################
 # setwd("C:/YOUR_PREFERRED_WORKING_DIRECTORY")
+#setwd("C:/Users/Helen/Documents/EBG")
+setwd("C:/Users/G13519/Documents/EB/Ejercicio DT/Mayo 2017")
 
 #Required libraries
 
+rm(list = ls()) #remove all previous work
+
 library(data.table)
+library(plyr)
 library(dplyr)
 library(reshape2)
 library(ggplot2)
@@ -21,7 +25,7 @@ download.file(fileURL,destfile="./polls17.tsv")
 # Full Polling Dataset (via HuffPost poll agregator)
 
 poll<-read.table("./polls17.tsv", sep = '\t', header = TRUE)
-surveyHouses<-data.frame(levels(poll$survey_house)) #names of all surveyers
+surveyHouses<-levels(poll$survey_house) #names of all surveyers
 
 #Subset (All Adults) some publish "Likely Voters" or "Registered Voters" instead of Adults
 pollsub <- subset(poll, poll$sample_subpopulation== "Adults" | poll$sample_subpopulation=="Likely Voters" | poll$sample_subpopulation=="Registered Voters")
@@ -33,7 +37,7 @@ pollAp_sh<-summaryBy(Approve ~ survey_house, data=pollsub,
 shm<-pollAp_sh[order(pollAp_sh$Approve.mean),] #order by min to max mean
 dmean<-shm[,1:2]
 colnames(dmean)<-c("Survey_House", "Approval")
-mean$Survey_House <- factor(dmean$Survey_House, levels = dmean$Survey_House[order(dmean$Approval)]) #ordenar factores para obtener colores ordenados
+dmean$Survey_House <- factor(dmean$Survey_House, levels = dmean$Survey_House[order(dmean$Approval)]) #ordenar factores para obtener colores ordenados
 
 # Bar plot
 library(RColorBrewer)
@@ -65,9 +69,9 @@ polls<-data.frame(pollsub$end_date)
 listdf <- list()
 i<-c(1:k)
 for (i in 1:k){
-        polls<- subset(pollsub, pollsub$survey_house==surveyHouses[i])
-        listdf[[i]] <-polls
-        assign(paste('SH',i,sep=''),polls)
+    polls<- subset(pollsub, pollsub$survey_house==surveyHouses[i])
+    listdf[[i]] <-polls
+    assign(paste('SH',i,sep=''),polls)
 }
 
 Gallup<-SH8[,c("end_date", "Approve")]
@@ -83,26 +87,27 @@ df<-data.frame(df)
 colnames(df)<-c("Date", "Gallup","YG_Economist","Ipsos_Reuters","SurveyMonkey", "Politico", "Rasmussen")
 
 for (j in 3:7){
-        df[,j]<-na.approx(df[,j], na.rm = FALSE)
+    df[,j]<-na.approx(df[,j], na.rm = FALSE)
 }
 
 df<-melt(df, id="Date")
 
 colnames(df) <- c("Date","SurveyHouse", "Approval")
 polpalette2<-polpalette[c(11,13,19,22,25,28)] #main sh colors
+
 soft<-0.15
 gM <- ggplot(df, aes(x = as.Date(Date),y = Approval, group=SurveyHouse, colour= SurveyHouse)) +
-        scale_color_manual(values=polpalette2)+
-        #geom_line(size=1, alpha=1)+
-        geom_smooth(size=1, alpha=1,method="loess", span=soft, se=FALSE)+ #loess=Polynomial Regression Fitting
-        coord_cartesian(ylim=c(30,60))+scale_x_date(date_breaks = "1 week")+
-        ggtitle("Approval Rating by Main Survey Houses (smoothed avg., %)")+
-        theme(plot.title = element_text(family = "Trebuchet", size=15,colour = "lightskyblue4",face="bold"),
-              panel.grid.major = element_line(colour = "white"),
-              panel.grid.minor = element_line(colour = "white"),
-              axis.text.x =element_text(size  = 8,angle = 90, hjust = 1,  vjust = 1),
-              axis.text.y =element_text(size  = 10))+
-        labs(x="Date",y="Approval (%)")
+    scale_color_manual(values=polpalette2)+
+    #geom_line(size=1, alpha=1)+
+    geom_smooth(size=1, alpha=1,method="loess", span=soft, se=FALSE)+ #loess=Polynomial Regression Fitting
+    coord_cartesian(ylim=c(30,60))+scale_x_date(date_breaks = "1 week")+
+    ggtitle("Approval Rating by Main Survey Houses (smoothed avg., %)")+
+    theme(plot.title = element_text(family = "Trebuchet", size=15,colour = "lightskyblue4",face="bold"),
+          panel.grid.major = element_line(colour = "white"),
+          panel.grid.minor = element_line(colour = "white"),
+          axis.text.x =element_text(size  = 8,angle = 90, hjust = 1,  vjust = 1),
+          axis.text.y =element_text(size  = 10))+
+    labs(x="Date",y="Approval (%)")
 gM
 
 ggsave(filename="App_by_Main_Survey_House.jpg", width=11, height=7)
